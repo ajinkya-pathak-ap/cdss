06e1b7488665345d7814cf058432a41a4f75661c
Ajinkya Mohanrao Pathak <AjinkyaP3@citiustech.com>
2021-11-30 13:11:16 +0530

    Restored Prev Verion of the AMI CDS Input

--- src/components/ami/AMICDSInput.js
+++ src/components/ami/AMICDSInput.js
@@ -22,13 +22,12 @@ import Select from "@mui/material/Select";
 import "./styles.css";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    backgroundColor: "#6D7F9B",
-    padding: "30px",
-    margin: "4%",
+  firstform: {
+    "& .MuiFormControl-root": {
+      width: "70%",
+      margin: theme.spacing(1),
+    },
   },
-
   secondform: {
     "& .MuiFormControl-root": {
       width: "70%",
@@ -37,22 +36,12 @@ const useStyles = makeStyles((theme) => ({
   },
 
   formgrop: {
-    padding: theme.spacing(2),
     textAlign: "left",
     color: "white",
     backgroundColor: "#050038",
     alignItems: "flex-start",
-    marginLeft: "15px",
   },
 
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: "left",
-    color: "white",
-    backgroundColor: "#050038",
-    alignItems: "flex-start",
-    marginLeft: "17px",
-  },
   buttonColor: {
     backgroundColor: "#414bb2",
     color: "#fff",
@@ -62,10 +51,7 @@ const useStyles = makeStyles((theme) => ({
   },
   textFieldColor: {
     backgroundColor: "white",
-    border: "solid 1px red",
-    "& .MuiInputLabel-root.Mui-focused": {
-      color: "red",
-    },
+    textAlign: "right",
   },
 }));
 
@@ -116,11 +102,11 @@ const symptoms = [
 const time = [
   {
     value: "7",
-    label: "tttttttt",
+    label: "1 Hrs",
   },
   {
     value: "8",
-    label: "tttttttt",
+    label: "2 Hrs",
   },
 ];
 
@@ -131,6 +117,7 @@ export default function AMICSInput() {
 
   const onTextChange = () => setTextValue();
 
+  /******methods of checkbox********/
   const label = {
     inputProps: { "aria-label": "Checkbox demo" },
   };
@@ -144,251 +131,269 @@ export default function AMICSInput() {
   };
 
   return (
-    <div className={classes.root}>
-      <Grid container spacing={2}>
-        <Grid item container xs={6} spacing={2}>
-          <Grid item xs={12} className={classes.formgrop}>
-            <form className={classes.secondform}>
-              <Typography style={{ textAlign: "left" }}>
-                Patient History
-              </Typography>
-              <Grid container>
-                <Grid item xs={12}>
-                  <h5>Demographics Information pulled from EHR</h5>
+    <Grid
+      md={12}
+      xs={12}
+      item
+      container
+      spacing={4}
+      className={classes.root}
+      style={{ backgroundColor: "#6D7F9B", padding: "30px" }}
+    >
+      <Grid
+        item
+        container
+        md={6}
+        xs={12}
+        style={{ backgroundColor: "#050038" }}
+      >
+        <form className={classes.firstform}>
+          <h5 style={{ textAlign: "left", color: "white" }}>Patient History</h5>
+          <h5 style={{ textAlign: "left", color: "white" }}>
+            Demographics Information pulled from EHR
+          </h5>
 
-                  <Box
-                    component="form"
-                    sx={{
-                      "& > :not(style)": { m: 1, width: "25ch" },
-                    }}
-                    noValidate
-                    autoComplete="off"
-                  >
-                    <TextField
-                      className={classes.textFieldColor}
-                      id="age"
-                      label="Patient's Age"
-                      variant="standard"
-                      value="55"
-                    />
+          <TextField
+            className={classes.textFieldColor}
+            id="age"
+            label="Age"
+            variant="standard"
+            type="number"
+          />
+          <TextField
+            className={classes.textFieldColor}
+            id="gender"
+            variant="standard"
+            select
+            label="Gender"
+            // onChange={handleChange1}
+          >
+            {Gender.map((option) => (
+              <MenuItem value={option.value}>{option.label}</MenuItem>
+            ))}
+          </TextField>
 
-                    <TextField
-                      className={classes.textFieldColor}
-                      id="gender"
-                      variant="outlined"
-                      select={true}
-                      fullWidth
-                      SelectProps={{
-                        MenuProps: {
-                          anchorOrigin: {
-                            vertical: "bottom",
-                            horizontal: "center",
-                          },
-                          getContentAnchorEl: null,
-                        },
-                      }}
-                      label="Gender"
-                      // onChange={handleChange1}
-                    >
-                      {Gender.map((option) => (
-                        <MenuItem key={option.value} value={option.value}>
-                          {option.label}
-                        </MenuItem>
-                      ))}
-                    </TextField>
+          <TextField
+            className={classes.textFieldColor}
+            id="Race"
+            select
+            label="Race"
+            variant="standard"
 
-                    <TextField
-                      className={classes.textFieldColor}
-                      id="Race"
-                      select={true}
-                      label="Race"
-                      // onChange={handleChange1}
-                    >
-                      {Race.map((option) => (
-                        <MenuItem key={option.value} value={option.value}>
-                          {option.label}
-                        </MenuItem>
-                      ))}
-                    </TextField>
+            // onChange={handleChange1}
+          >
+            {Race.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
 
-                    <TextField
-                      className={classes.textFieldColor}
-                      id="symptoms"
-                      select
-                      label="Presenting Symptoms"
-                      // onChange={handleChange1}
-                    >
-                      {symptoms.map((option) => (
-                        <MenuItem key={option.value} value={option.value}>
-                          {option.label}
-                        </MenuItem>
-                      ))}
-                    </TextField>
+          <TextField
+            className={classes.textFieldColor}
+            id="symptoms"
+            select
+            label="Presenting Symptoms"
+            variant="standard"
 
-                    <TextField
-                      className={classes.textFieldColor}
-                      id="time"
-                      select
-                      label="Time since symptom onset"
-                      // onChange={handleChange1}
-                    >
-                      {time.map((option) => (
-                        <MenuItem key={option.value} value={option.value}>
-                          {option.label}
-                        </MenuItem>
-                      ))}
-                    </TextField>
+            // onChange={handleChange1}
+          >
+            {symptoms.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
 
-                    <TextField
-                      className={classes.textFieldColor}
-                      id="risk factors"
-                      select
-                      label="Risk Factors"
-                      // onChange={handleChange1}
-                    >
-                      {riskFactor.map((option) => (
-                        <MenuItem key={option.value} value={option.value}>
-                          {option.label}
-                        </MenuItem>
-                      ))}
-                    </TextField>
-                  </Box>
-                </Grid>
-              </Grid>
-            </form>
-          </Grid>
-        </Grid>
+          <TextField
+            className={classes.textFieldColor}
+            id="time"
+            select
+            label="Time since symptom onset"
+            variant="standard"
 
-        <Grid xs={6} container spacing={2}>
-          {/* *****************  first Box *********************** */}
-          <Grid item xs={12}>
-            <FormGroup className={classes.formgrop}>
-              <Typography className={classes.typo}>
-                EKG Findings(Select One)
-              </Typography>
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    {...label}
-                    sx={{
-                      color: "#fff",
-                      "&.Mui-checked": {
-                        color: "#fff",
-                      },
-                    }}
-                  />
-                }
-                label="No ST deviation, but LBBB, LVH, repolarization changes "
-              />
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    {...label}
-                    sx={{
-                      color: "#fff",
-                      "&.Mui-checked": {
-                        color: "#fff",
-                      },
-                    }}
-                  />
-                }
-                label="ST deviation, but LBBB, LVH, repolarization changes"
-              />
-            </FormGroup>
-          </Grid>
+            // onChange={handleChange1}
+          >
+            {time.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
 
-          {/* ***************** start of second Box *********************** */}
-          <form className={classes.secondform}>
-            <Grid container className={classes.formgrop}>
-              <Grid item xs={6}>
-                <Box
-                  component="form"
-                  sx={{
-                    "& > :not(style)": { m: 1, width: "25ch" },
-                  }}
-                  noValidate
-                  autoComplete="off"
-                >
-                  <h4>hsTnl Results</h4>
+          <TextField
+            className={classes.textFieldColor}
+            id="risk factors"
+            select
+            label="Risk Factors"
+            variant="standard"
 
-                  <TextField
-                    className={classes.textFieldColor}
-                    id="25 ng/L"
-                    label="25 ng/L"
-                    variant="outlined"
-                  />
-
-                  <TextField
-                    className={classes.textFieldColor}
-                    id="35 ng/L"
-                    label="35 ng/L"
-                    variant="outlined"
-                  />
-
-                  <TextField
-                    className={classes.textFieldColor}
-                    id=" ng/L"
-                    label="ng/L"
-                    variant="outlined"
-                  />
-                </Box>
-              </Grid>
-
-              <Grid item xs={6}>
-                <h4>Draw Time</h4>
-                <TextField
-                  className={classes.textFieldColor}
-                  id="date1"
-                  label="10/26/2021,20:40"
-                  variant="outlined"
-                />
-
-                <TextField
-                  className={classes.textFieldColor}
-                  id="date2"
-                  label="10/26/2021,23:10"
-                  variant="outlined"
-                />
-                <TextField
-                  className={classes.textFieldColor}
-                  id="date3"
-                  label="mm:hh:yyyy"
-                  variant="outlined"
-                />
-              </Grid>
-            </Grid>
-          </form>
-
-          {/* *****************  start third Box *********************** */}
-          <Grid item xs={12}>
-            {/* <Paper className={classes.paper}>1</Paper> */}
-            <Stack xs={12} direction="row" justifyContent="center" spacing={2}>
-              <Button
-                className={classes.buttonColor}
-                variant="contained"
-                style={{ textTransform: "none", backgroundColor: "#414BB2" }}
-              >
-                Exit
-              </Button>
-              <Button
-                className={classes.buttonColor}
-                variant="contained"
-                style={{ textTransform: "none", backgroundColor: "#414BB2" }}
-              >
-                Reset
-              </Button>
-              <Button
-                className={classes.buttonColor}
-                style={{ textTransform: "none", backgroundColor: "#414BB2" }}
-                variant="contained"
-                href="#contained-buttons"
-              >
-                Calculate Risk Score
-              </Button>
-            </Stack>
-          </Grid>
-        </Grid>
+            // onChange={handleChange1}
+          >
+            {riskFactor.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
+          {/* </Box> */}
+          {/* </Grid> */}
+          {/* </Grid> */}
+        </form>
+        {/* </Grid> */}
       </Grid>
-    </div>
+
+      <Grid
+        item
+        container
+        spacing={4}
+        md={6}
+        xs={12}
+        style={{ backgroundColor: "#6D7F9B" }}
+      >
+        {/* *****************  first Box *********************** */}
+        <Grid
+          item
+          xs={12}
+          md={12}
+          style={{ backgroundColor: "#050038", paddingLeft: "16px" }}
+        >
+          <FormGroup className={classes.formgrop}>
+            <h5 style={{ textAlign: "left", color: "white" }}>
+              EKG Findings(Select One)
+            </h5>
+
+            <FormControlLabel
+              control={
+                <Checkbox
+                  {...label}
+                  sx={{
+                    color: "#fff",
+                    "&.Mui-checked": {
+                      color: "#fff",
+                    },
+                  }}
+                />
+              }
+              label="No ST deviation, but LBBB, LVH, repolarization changes "
+            />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  {...label}
+                  sx={{
+                    color: "#fff",
+                    "&.Mui-checked": {
+                      color: "#fff",
+                    },
+                  }}
+                />
+              }
+              label="ST deviation, but LBBB, LVH, repolarization changes"
+            />
+          </FormGroup>
+        </Grid>
+        {/* ***************** end of first Box *********************** */}
+
+        {/* ***************** start of second Box *********************** */}
+        <form className={classes.secondform}>
+          <Grid
+            container
+            className={classes.formgrop}
+            style={{ paddingLeft: "16px" }}
+          >
+            <Grid item xs={6}>
+              <Box
+                component="form"
+                sx={{
+                  "& > :not(style)": { m: 1, width: "25ch" },
+                }}
+                noValidate
+                autoComplete="off"
+              >
+                <h4>HsTnl Results</h4>
+
+                <TextField
+                  className={classes.textFieldColor}
+                  id="25 ng/L"
+                  label="25 ng/L"
+                  variant="outlined"
+                />
+
+                <TextField
+                  className={classes.textFieldColor}
+                  id="35 ng/L"
+                  label="35 ng/L"
+                  variant="outlined"
+                />
+
+                <TextField
+                  className={classes.textFieldColor}
+                  id=" ng/L"
+                  label="ng/L"
+                  variant="outlined"
+                />
+              </Box>
+            </Grid>
+
+            <Grid item xs={6}>
+              <h4>Draw Time</h4>
+              <TextField
+                className={classes.textFieldColor}
+                id="date1"
+                label="10/26/2021,20:40"
+                variant="outlined"
+              />
+
+              <TextField
+                className={classes.textFieldColor}
+                id="date2"
+                label="10/26/2021,23:10"
+                variant="outlined"
+              />
+              <TextField
+                className={classes.textFieldColor}
+                id="date3"
+                label="mm:hh:yyyy"
+                variant="outlined"
+              />
+            </Grid>
+          </Grid>
+        </form>
+        {/* ***************** end of second Box *********************** */}
+
+        {/* ***************** end of second Box *********************** */}
+
+        {/* *****************  start third Box *********************** */}
+        <Grid item xs={12}>
+          {/* <Paper className={classes.paper}>1</Paper> */}
+          <Stack xs={12} direction="row" justifyContent="center" spacing={2}>
+            <Button
+              className={classes.buttonColor}
+              variant="contained"
+              style={{ textTransform: "none", backgroundColor: "#414BB2" }}
+            >
+              Exit
+            </Button>
+            <Button
+              className={classes.buttonColor}
+              variant="contained"
+              style={{ textTransform: "none", backgroundColor: "#414BB2" }}
+            >
+              Reset
+            </Button>
+            <Button
+              className={classes.buttonColor}
+              style={{ textTransform: "none", backgroundColor: "#414BB2" }}
+              variant="contained"
+              href="#contained-buttons"
+            >
+              Calculate Risk Score
+            </Button>
+          </Stack>
+        </Grid>
+        {/* ***************** end of third Box *********************** */}
+      </Grid>
+    </Grid>
   );
 }
