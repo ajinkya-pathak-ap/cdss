5cbc4bd0b218c4e22cf7e040f217401895b2ac5e
Susmitha1281 <Susmitha.Chinthakindi@citiustech.com>
2022-01-11 11:10:06 +0530

    changes in amiinput

--- src/components/ami/amiinput/calculateriskscore/CalculateScoreStyles.js
+++ src/components/ami/amiinput/calculateriskscore/CalculateScoreStyles.js
@@ -1,14 +1,15 @@
 import { makeStyles, styled, InputBase } from "@material-ui/core";
 
-export const CalculateScoreStyles = makeStyles((theme) => ({
+export const btnStyles = makeStyles((theme) => ({
   buttonColor: {
     backgroundColor: "#414bb2",
     color: "#fff",
     "&:hover": {
       backgroundColor: "#414BB2",
     },
+    paddingLeft: "25px",
+    paddingRight: "25px",
     textTransform: "none",
-    backgroundColor: "#414BB2",
   },
 
   riskScore1: {
@@ -25,25 +26,5 @@ export const CalculateScoreStyles = makeStyles((theme) => ({
       padding: "10px !important",
       marginLeft: "4px !important",
     },
-  },
-}));
-
-export const bootstrappedInput = styled(InputBase)(({ theme }) => ({
-  "label + &": {
-    marginTop: theme.spacing(3),
-  },
-  "& .MuiInputBase-input": {
-    borderRadius: 4,
-    position: "relative",
-    backgroundColor: theme.palette.mode === "light" ? "#fcfcfb" : "#2b2b2b",
-    fontSize: 13,
-    width: "auto",
-    padding: "9px 10px",
-    transition: theme.transitions.create([
-      "border-color",
-      "background-color",
-      "box-shadow",
-    ]),
-    fontFamily: ["Roboto"].join(","),
   },
 }));
